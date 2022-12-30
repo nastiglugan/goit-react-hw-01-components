1f01332c03cd1faa8d80a8af240eb049bce72e2c
NastiHluhan <0506167197more@gmail.com>
2022-12-30 23:01:16 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import profileInfo from '';
+import { Profile } from '';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Profile />;
 };
+
+// <div
+//   style={{
+//     height: '100vh',
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     fontSize: 40,
+//     color: '#010101',
+//   }}
+// >
+//   React homework template
+// </div>
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return <div></div>;
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
